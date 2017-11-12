bb51a2b50496810b95f2a5453a4b899efaadf9e1
Andre Masao Terasaka <andre.masao@outlook.com>
2017-11-12 19:59:02 -0200

    Adicionando logs e melhorando o codigo

--- ip.rb
+++ ip.rb
@@ -2,24 +2,30 @@ require './def.rb'
 require 'logger'
 require 'socket'
 
-# Pega o usuario logado e o IP
+# Pega o usuario logado, IP e HOSTNAME
 @user = `whoami`.strip
-@ip_address = Socket.ip_address_list.find { |ai| ai.ipv4? && !ai.ipv4_loopback? }.ip_address
+@logger.debug "Usuario logado #{@user}"
+
+@ip_address = Socket.ip_address_list.find { |lista| lista.ipv4? && !lista.ipv4_loopback? }.ip_address
+@hostname = Socket.gethostname
+@logger.debug "IP do usuario logado: #{@ip_address}"
+@logger.debug "HOSTNAME do usuario logado: #{@hostname}"
 
 checa_arquivo_ip
 
-if (ARGV[0] == "-list") && (ARGV[1] == "dev")
+@logger.debug "Opcao digitada pelo usuario #{ARGV[0]} / #{ARGV[1]}"
+if (ARGV[0].downcase == "-list") && (ARGV[1].downcase == "dev")
     lista_ips
-        elsif (ARGV[0] == "-new") && (ARGV[1] == "dev")
+        elsif (ARGV[0].downcase == "-new") && (ARGV[1].downcase == "dev")
             # Le o arquivo
             ip = File.read("ip_disponivel")
-            if (ip.empty? == false)
+            if (!ip.empty?)
             # Transforma num array
             disponiveis = ip.split
             # Reserva um IP e remove do Array
             reserva = disponiveis.pop
             puts "Para a Vlan #{ARGV[1]} utilize o IP #{reserva}"
-            puts "Usuario/IP do solicitante #{@user} / #{@ip_address}"
+            @logger.info "IP #{reserva} reservado para Login: #{@user} / IP: #{@ip_address} / HOSTNAME: #{@hostname}"
             # Grava o Array no arquivo após o endereço removido pelo pop
             temp = File.open("ip_disponivel", "w") do |file|
                 file.puts disponiveis
@@ -28,12 +34,13 @@ if (ARGV[0] == "-list") && (ARGV[1] == "dev")
             # Escreve no arquivo o ip reservado e o login que solicitou
             temp = File.open("ip_uso", "a") do |file|
                 file.puts "#{reserva} - #{@user}"
+                @logger.info "IP #{reserva} em uso registrado no arquivo ip_uso"
             end
             
             else
                 puts "Acabaram os IPs seu animal"
+                @logger.error "Acabaram os IPs disponiveis em ip_disponivel"
             end
-        #puts "Pega esse ip aqui #{file[0]} da vlan #{ARGV[1]}"
         else 
             erro_opcao
 end
